9bf79ace53483dc1dd2f66a80f239397ca0126a4
Luis Valentini <luisfernandocvalentini@gmail.com>
2025-11-26 13:01:48 -0300

    fix: ajustado intervalo de tempo em que o job de ingestão será executado. Logs da task padronizados

--- backend/tasks/ingestion.js
+++ backend/tasks/ingestion.js
@@ -4,18 +4,33 @@ import { MIN_RATING, MIN_VOTES } from '../util/constants.js';
 import tmdbAPI from '../api/tmdb-api.js';
 import { fetchAndSanitizeMovies } from '../util/api-util.js';
 
-
+// inserting newly released movies to the database. shouldn't take longer than a few seconds
 async function ingestRecentMovies() {
-  console.log("starting task - " + new Date().toISOString());
+  const now = new Date();
+  console.log(now.toISOString() + " - [ingestion task]: Starting task.");
 
-  const min_date = new Date();
-  min_date.setMonth(min_date.getMonth() - 3);
-  const dateStr = `${min_date.getFullYear()}-${String(min_date.getMonth() + 1).padStart(2, '0')}-${String(min_date.getDate()).padStart(2, '0')}`;
+  now.setMonth(now.getMonth() - 3);
+  const dateStr = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
   // try to get movies that meet the requirements from the last 3 months
   const url = `/discover/movie?include_adult=false&language=en-US&vote_count.gte=${MIN_VOTES}&vote_average.gte=${MIN_RATING}&primary_release_date.gte=${dateStr}`;
 
-  console.log(url);
+  async function getPages() {
+    try {
+      const response = await tmdbAPI.get(url);
+      const data = response.data;
+      const { total_pages } = data;
+      if (!total_pages) throw new Error();
+      return total_pages;
+    } catch (err) {
+      console.error(new Date().toISOString() + ' - [ingestion task]: Failed to get number of pages; execution canceled.');
+      return null;
+    }
+  }
 
+  const pages = await getPages();
+  if (!pages) return;
+
+  let successCount = 0;
   const movie_statement = `
     INSERT INTO movie(id, title, poster_path, year, tmdb_rating)
     VALUES ($1, $2, $3, $4, $5);
@@ -26,50 +41,33 @@ async function ingestRecentMovies() {
     VALUES ($1, $2);
   `;
 
-  async function getPages() {
-    try {
-      const response = await tmdbAPI.get(url);
-      const data = response.data;
-      const { total_pages } = data;
-      if (!total_pages) throw new Error();
-      return total_pages;
-    } catch (err) {
-      const now = new Date().toISOString();
-      console.error(now + ' - getting total pages of ingestion failed - execution canceled.');
-      return null;
-    }
-  }
-
-  const pages = await getPages();
-  if (!pages) return;
-
   for (let page = 1; page <= pages; page++) {
     try {
       const movies = await fetchAndSanitizeMovies(url + '&page=' + page);
-      // try to insert one by one. If it's duplicate, db trigger catches it.
 
+      // try to insert one by one. If it's duplicate, db trigger catches it.
       movies.forEach(async (movie) => {
         const { id, title, poster_path, year, tmdb_rating } = movie;
         if (!id || !title) return;
         const args = [id, title, poster_path, (year === 'N/A') ? null : year, (tmdb_rating === 'N/A') ? null : tmdb_rating];
         const error = await tryInsert(movie_statement, args);
         if (error) return;
+        successCount++;
         const genres = movie.genre_ids;
         if (Array.isArray(genres)) genres.forEach(async (genre) => tryInsert(genre_statement, [id, genre]));
+        console.log(genres);
       });
     } catch (err) {
-      const now = new Date().toISOString();
-      console.log(now + ' - Failed to get movies: page ' + page);
+      console.log(new Date().toISOString() + ' - [ingestion task]: Failed to get movie page #' + page);
       console.log(err);
     }
   }
 
-  console.log('ending task - ' + new Date().toISOString());
+  console.log(new Date().toISOString() + ' - [ingestion task]: Ending task; ' + successCount + " movie(s) added successfully.");
 }
 
-// runs every 1st of the month at 12 AM
-// '0 0 1 * *'
 
 export default function scheduleJobs() {
-  cron.schedule('25 51 18 24 11 *', ingestRecentMovies);
+  // runs every 1st and 15th of the month at 12 AM
+  cron.schedule('0 0 1,15 * *', ingestRecentMovies);
 }
